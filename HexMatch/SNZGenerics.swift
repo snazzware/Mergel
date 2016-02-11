210a45e411a9b65f736d3272a46fe3a81756dede
Josh McKee <josh@snazzware.com>
2016-02-10 20:22:36 -0600

    generic stack struct

---
+++ HexMatch/SNZGenerics.swift
@@ -0,0 +1,35 @@
+//
+//  SNZGenerics.swift
+//  HexMatch
+//
+//  Created by Josh McKee on 2/10/16.
+//  Copyright © 2016 Josh McKee. All rights reserved.
+//
+
+import Foundation
+
+struct Stack<Element> {
+    var items = [Element]()
+    
+    var count: Int {
+        get {
+            return self.items.count
+        }
+    }
+    
+    mutating func push(item: Element) {
+        self.items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        return self.items.removeLast()
+    }
+    
+    mutating func clear() {
+        self.items.removeAll()
+    }
+    
+    mutating func reverseInPlace() {
+        self.items = self.items.reverse()
+    }
+}
