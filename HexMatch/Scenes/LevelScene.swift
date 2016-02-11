15e4ba6df1abdf02ca5d9141a6725798e8742fff
Josh McKee <josh@snazzware.com>
2016-02-10 20:18:02 -0600

    refactoring textures, improving positioning

--- HexMatch/Scenes/LevelScene.swift
+++ HexMatch/Scenes/LevelScene.swift
@@ -14,7 +14,9 @@ class LevelScene: SNZScene {
     var checkboxMobilePieces: SNZCheckButtonWidget?
     var checkboxEnemyPieces: SNZCheckButtonWidget?
     
-    override func didMoveToView(view: SKView) {        
+    override func didMoveToView(view: SKView) {
+        super.didMoveToView(view)
+        
         self.updateGui()
     }
     
@@ -31,12 +33,12 @@ class LevelScene: SNZScene {
         self.addWidget(header)
         
         
-        var verticalOffset:CGFloat = self.frame.height - 100
+        var verticalOffset:CGFloat = self.frame.height - 120
         
         // Create and position level buttons
         for unlockedLevel in (GameState.instance!.unlockedLevels) {
             let levelButton = SNZButtonWidget()
-            levelButton.position = CGPointMake(10,verticalOffset)
+            levelButton.position = CGPointMake(20,verticalOffset)
             levelButton.caption = LevelHelper.instance.getLevelHelperModeCaption(unlockedLevel)
             levelButton.bind("tap",{
                 self.captureSettings()
@@ -53,24 +55,21 @@ class LevelScene: SNZScene {
         
         // Create and position option buttons
         
-        verticalOffset = self.frame.height - 100
+        let horizontalOffset:CGFloat = self.frame.width < 500 ? 20 : 300
+        verticalOffset = self.frame.width < 500 ? verticalOffset - 60 : self.frame.height - 120
         
         self.checkboxMobilePieces = SNZCheckButtonWidget(parentNode: self)
         self.checkboxMobilePieces!.isChecked = GameState.instance!.getIntForKey("include_mobile_pieces", 1) == 1
-        self.checkboxMobilePieces!.caption = "Mobile Pieces"
-        self.checkboxMobilePieces!.position = CGPointMake(300,verticalOffset)
-        self.checkboxMobilePieces!.checkedTexture = SKTexture(imageNamed: "checkboxChecked")
-        self.checkboxMobilePieces!.uncheckedTexture = SKTexture(imageNamed: "checkboxUnchecked")
+        self.checkboxMobilePieces!.caption = "Moving Shapes"
+        self.checkboxMobilePieces!.position = CGPointMake(horizontalOffset,verticalOffset)
         self.addWidget(self.checkboxMobilePieces!)
         
         verticalOffset -= 60
         
         self.checkboxEnemyPieces = SNZCheckButtonWidget(parentNode: self)
         self.checkboxEnemyPieces!.isChecked = GameState.instance!.getIntForKey("include_enemy_pieces", 1) == 1
-        self.checkboxEnemyPieces!.caption = "Enemy Pieces"
-        self.checkboxEnemyPieces!.position = CGPointMake(300,verticalOffset)
-        self.checkboxEnemyPieces!.checkedTexture = SKTexture(imageNamed: "checkboxChecked")
-        self.checkboxEnemyPieces!.uncheckedTexture = SKTexture(imageNamed: "checkboxUnchecked")
+        self.checkboxEnemyPieces!.caption = "Vanilla Gel"
+        self.checkboxEnemyPieces!.position = CGPointMake(horizontalOffset,verticalOffset)
         self.addWidget(self.checkboxEnemyPieces!)
         
         // Add the close button
